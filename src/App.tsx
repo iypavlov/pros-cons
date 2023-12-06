ba7b727f0cd4db685ce43dbdafd533fa45bad9e8
iypavlov <iy.pavlov@yandex.ru>
2023-12-06 19:34:49 +0300

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import './assets/styles/index.scss';
 import { Layout } from './components/Layout/Layout';
 import { PAGES_CONFIG } from './constants/pages';
@@ -7,29 +7,32 @@ import { Home } from './pages/Home/Home';
 import { Ticket } from './pages/Ticket/Ticket';
 import { Tickets } from './pages/Tickets/Tickets';
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: PAGES_CONFIG.home.path,
-        element: <Home />,
-      },
-      {
-        path: PAGES_CONFIG.tickets.path,
-        element: <Tickets />,
-      },
-      {
-        path: PAGES_CONFIG.ticket.path,
-        element: <Ticket />,
-      },
-      {
-        path: PAGES_CONFIG.createTicket.path,
-        element: <CreateTicket />,
-      },
-    ],
-  },
-]);
+const router = createHashRouter(
+  [
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: PAGES_CONFIG.home.path,
+          element: <Home />,
+        },
+        {
+          path: PAGES_CONFIG.tickets.path,
+          element: <Tickets />,
+        },
+        {
+          path: PAGES_CONFIG.ticket.path,
+          element: <Ticket />,
+        },
+        {
+          path: PAGES_CONFIG.createTicket.path,
+          element: <CreateTicket />,
+        },
+      ],
+    },
+  ],
+  { basename: '/' }
+);
 
 function App() {
   return <RouterProvider router={router} />;
